acb056f112c051fc7b3254a844526e6dac8bf07a
gryf <gryf73@gmail.com>
2016-12-19 20:23:27 +0100

    Added plain module - for running fs-uae directly

---
+++ fs_uae_wrapper/plain.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Simple class for executing fs-uae with specified parameters. This is a
+failsafe class for running fs-uae.
+"""
+import subprocess
+import sys
+
+
+class Plain(object):
+    """Class for run fs-uae"""
+
+    def run(self, conf, fs_uae_options):
+        """
+        Run the emulation.
+        conf is a path to the configuration,
+        fs_uae_options is a list contains tokenized options to be passed to
+                       fs-uae
+        """
+        try:
+            subprocess.call(['fs-uae'] + [conf] + fs_uae_options)
+        except subprocess.CalledProcessError:
+            sys.stderr.write('Warning: fs-uae returned non 0 exit code\n')
+
+    def clean(self):
+        """In this class, do nothing on exit"""
+        return
+
+
+def run(config_file, fs_uae_options, _):
+    """Run fs-uae with provided config file and options"""
+
+    runner = Plain()
+    try:
+        runner.run(config_file, fs_uae_options)
+    finally:
+        runner.clean()
